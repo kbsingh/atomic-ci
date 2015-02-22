ebab0a8d35d1f8b3a5535b306cf20b391ca35dff
Karanbir Singh <kbsingh@karan.org>
2015-02-22 01:01:32 +0000

    setup new test to download the atomic host vm

---
+++ tests/p_1-getAtomicHost/0-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+t_Log "$0 - Downloading Atomic Host image"
+
+if [ $centos_ver -lt 6 ]; then
+  t_Log "This test suite will only run on CentOS 6 or CentOS 7. Aborting!"
+  t_CheckExitStatus 1
+  exit $FAIL
+fi
+
+# make sure we have enough diskspace to make this work 
+dspace=$(df . | awk '/[0-9]%/{print $(NF-2)}')
+if [ $dspace -lt 8388608 ]; then
+  echo 'We need atleast 4GB of disk space to make this work. Abort!'
+  t_CheckExitStatus 1
+  exit $FAIL
+fi
